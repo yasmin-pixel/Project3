852fd0f2007d413012d716e47f241eaf0a240a48
yasmin-pixel <65359374+yasmin-pixel@users.noreply.github.com>
2022-01-18 16:55:24 +0000

    added two more routes

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,8 @@ const jwt = require("jsonwebtoken");
 const { Track } = require("../models/track");
 const { Genre } = require("../models/genre");
 const { User } = require("../models/user");
+const { Album } = require("../models/albums");
+const { Artist } = require("../models/artists");
 
 const jwtOptions = {
   secretOrKey: "secret!!!",
@@ -84,6 +86,52 @@ router.get("/genres", (req, res) => {
       });
     } else {
       res.json(genres);
+    }
+  });
+});
+router.get("/albums/:id", (req, res) => {
+  Album.findById(req.params.id, function (error, album) {
+    if (error) {
+      res.status(400).json({
+        message: "album not found",
+      });
+    } else {
+      res.json(album);
+    }
+  });
+});
+
+router.post("/tracks", (req, res) => {
+  {
+    User.findMany({ username: req.body.username }, (error, user) => {
+      if (user) {
+        user.authenticate(req.body.password, (error, user) => {
+          if (error) {
+            res.status(401).json({ message: "invalid username or password" });
+          } else {
+            const unique_identifier = { username: user.username };
+            const token = jwt.sign(unique_identifier, jwtOptions.secretOrKey);
+            res.status(200).json({
+              message: `welcome, you have successfully sgined in, ${user.username}`,
+              token: token,
+            });
+          }
+        });
+      } else {
+        res.status(401).json({ message: "invalid username or password" });
+      }
+    });
+  }
+});
+
+router.get("/artists/:id", (req, res) => {
+  Artist.findById(req.params.id, function (error, artist) {
+    if (error) {
+      res.status(400).json({
+        message: "Artist found",
+      });
+    } else {
+      res.json(artist);
     }
   });
 });
